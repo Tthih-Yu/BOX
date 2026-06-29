04c0ecae8d1a1464146595849831df877d8faa88
雨中人 <2190638246@qq.com>
2026-06-29 08:20:26 +0800

    Use stable Android Gradle DSL for APK build

--- androidAPP/app/build.gradle.kts
+++ androidAPP/app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 
 android {
     namespace = "com.wanshijie.aptiv"
-    compileSdk {
-        version = release(37)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.wanshijie.aptiv"
@@ -26,15 +24,16 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlin {
-        jvmToolchain(17)
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
     }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
